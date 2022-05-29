6e341d4f2093195b7bc85fcd5272733daa79279d
Petr Zita <mgr.petr.zita@gmail.com>
2022-05-29 18:35:51 +0200

    Adding empty adbooleantypetest extension

---
+++ adbooleantypetest/adbooleantypetest/src/com/akhetdigital/constants/AdbooleantypetestConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.akhetdigital.constants;
+
+/**
+ * Global class for all Adbooleantypetest constants. You can add global constants for your extension into this class.
+ */
+public final class AdbooleantypetestConstants extends GeneratedAdbooleantypetestConstants
+{
+	public static final String EXTENSIONNAME = "adbooleantypetest";
+
+	private AdbooleantypetestConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
